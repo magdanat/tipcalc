cae30f356250500bd20815dd85176b3ab4e4e828
magdanat <nathanmagdalera@gmail.com>
2019-04-23 06:05:53 -0700

    Finish base assignment

--- app/src/main/java/edu/washington/magdanat/tipcalc/MainActivity.kt
+++ app/src/main/java/edu/washington/magdanat/tipcalc/MainActivity.kt
@@ -4,46 +4,76 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.widget.Button
-import android.widget.EditText
+import android.util.Log
+import android.widget.*
+import java.text.NumberFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    val TAG = "MainActivity"
+    var nonNum : Regex = Regex("[$,.]")
+    var money : String = "0.00"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // If edittext isn't empty, enable button
         val button: Button = findViewById(R.id.buttonTip)
-        val editText: EditText = findViewById(R.id.editText)
+        var editText: EditText = findViewById(R.id.editText)
+        // Reformats inputted text to a double that is reformatted into US currency
+        val moneyFormat = NumberFormat.getCurrencyInstance(Locale.US)
+
+        // Tip button is disabled on launch
+        button.isEnabled = false
 
         editText.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-//                if (editText.text.isEmpty()) {
-//                    button.isEnabled = false
-//                } else {
-//                    button.isEnabled = true
-//                }
-                button.isEnabled = editText.text.isEmpty()
-                if (!editText.text.isEmpty()) {
-
-                }
-            }
 
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
 
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            override fun onTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
+
+            override fun afterTextChanged(s: Editable) {
+                Log.i(TAG, "The input has changed!")
+
+                // Enables button
+                button.isEnabled = true;
+
+                // Remove listener to avoid crashing application
+                editText.removeTextChangedListener(this)
+
+                val temp = editText.text.toString().replace(nonNum, "").toDouble()
+                val new = moneyFormat.format((temp / 100))
+
+
+                editText.setText(new)
+
+                // Places selection at the end
+                editText.setSelection(editText.text.length)
+
+                // Will remove all text if user removes all number values and disable the button again
+                if (editText.text.toString().equals("$0.00")) {
+                    editText.setText("")
+                    button.isEnabled = false
+                }
+
+                // Adds listener back for new input changes
+                editText.addTextChangedListener(this)
+                money = new
+            }
+
         })
+
+        button.setOnClickListener {
+            Log.i(TAG, "The button has been clicked!")
+
+            val temp = money.replace(nonNum, "").toDouble()
+            val newNum = temp * 0.15
+            Toast.makeText(this, moneyFormat.format((newNum / 100)), Toast.LENGTH_SHORT).show()
+
+        }
     }
-
-    private fun amount() {
-
-    }
-
-    private fun tipButton() {
-
-    }
-
 }
+
