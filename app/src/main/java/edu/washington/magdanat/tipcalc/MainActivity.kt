5d69c79186a4891b7d487f4126eee393eb9b2524
magdanat <nathanmagdalera@gmail.com>
2019-04-23 14:25:24 -0700

    Refine code

--- app/src/main/java/edu/washington/magdanat/tipcalc/MainActivity.kt
+++ app/src/main/java/edu/washington/magdanat/tipcalc/MainActivity.kt
@@ -11,9 +11,6 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
     val TAG = "MainActivity"
-    var nonNum : Regex = Regex("[$,.]")
-    var money : String = "0.00"
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +18,7 @@ class MainActivity : AppCompatActivity() {
 
         val button: Button = findViewById(R.id.buttonTip)
         var editText: EditText = findViewById(R.id.editText)
+
         // Reformats inputted text to a double that is reformatted into US currency
         val moneyFormat = NumberFormat.getCurrencyInstance(Locale.US)
 
@@ -29,9 +27,11 @@ class MainActivity : AppCompatActivity() {
 
         editText.addTextChangedListener(object : TextWatcher {
 
+            // Needed to use listener
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
 
+            // Needed to use listener
             override fun onTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
 
@@ -44,12 +44,11 @@ class MainActivity : AppCompatActivity() {
                 // Remove listener to avoid crashing application
                 editText.removeTextChangedListener(this)
 
-                val temp = editText.text.toString().replace(nonNum, "").toDouble()
-                val new = moneyFormat.format((temp / 100))
-
+                // Call replaceStrings on editText to replace char values
+                val tempNum = replaceStrings(editText).toDouble()
+                val new = moneyFormat.format((tempNum / 100))
 
                 editText.setText(new)
-
                 // Places selection at the end
                 editText.setSelection(editText.text.length)
 
@@ -61,18 +60,23 @@ class MainActivity : AppCompatActivity() {
 
                 // Adds listener back for new input changes
                 editText.addTextChangedListener(this)
-                money = new
             }
         })
 
         button.setOnClickListener {
             Log.i(TAG, "The button has been clicked!")
 
-            val temp = money.replace(nonNum, "").toDouble()
-            val newNum = temp * 0.15
-            Toast.makeText(this, moneyFormat.format((newNum / 100)), Toast.LENGTH_SHORT).show()
+            val tempNum = replaceStrings(editText).toDouble() * 0.15
+            Toast.makeText(this, "Tip is " + moneyFormat.format((tempNum / 100)), Toast.LENGTH_SHORT).show()
 
         }
+    }
+
+    // Method to replace $ and . values
+    private fun replaceStrings(oldText: EditText) : String {
+        var text = oldText.text.toString().replace(".", "")
+        text = text.replace("$", "")
+        return text;
     }
 }
 
